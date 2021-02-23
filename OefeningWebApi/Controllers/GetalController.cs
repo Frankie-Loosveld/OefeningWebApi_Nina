df50eb8ed9d5cdfbfa666de6bab5b6aa07fc1abf
Nina <nina.roland@hotmail.com>
2021-02-23 15:59:40 +0100

    juuu

--- OefeningWebApi/Controllers/GetalController.cs
+++ OefeningWebApi/Controllers/GetalController.cs
@@ -1,16 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OefeningWebApi.Controllers
 {
-    public class GetalController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetalController : ControllerBase
     {
-        public IActionResult Index()
+        public GetalController()
         {
-            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult<int> GetGetal()
+        {
+            try
+            {
+                int g = Convert.ToInt32(System.IO.File.ReadAllText("Getal.txt"));
+                return Ok(g);
+            } catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost ("nieuw")]
+        public ActionResult NewGetal(int getal)
+        {
+            try
+            {
+                string g = Convert.ToString(getal);
+
+                System.IO.File.WriteAllText("Getal.txt", g);
+                return Ok();
+            } catch (Exception ex)
+            {
+                return NotFound();
+            }
+            
+        }
+
+        [HttpPost ("random")]
+        public ActionResult RandomGetal()
+        {
+            Random r = new Random();
+
+            int getal = r.Next(1, 10);
+
+            System.IO.File.WriteAllText("Getal.txt", Convert.ToString(getal));
+
+            return Ok();
         }
     }
 }

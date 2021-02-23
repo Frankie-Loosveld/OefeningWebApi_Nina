423e0d7a8ab3e34799f4fa8df6e07883355e876d
Nina <nina.roland@hotmail.com>
2021-02-23 14:09:24 +0100

    Add project files.

---
+++ OefeningWebApi/Controllers/GetalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OefeningWebApi.Controllers
+{
+    public class GetalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
